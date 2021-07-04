f7f1ffad4e80ac4493367f4f508339803bbdd541
donissousa <donisgtr@gmail.com>
2021-07-04 20:30:28 -0300

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-import express from "express";
+import express from "express"; /* ... pontinhos sigifina que instalar as tipagens como devDepencies  @types/nomedabiblioteca nesse caso yarn add @types/express*/
+
+const app = express();
+
+app.get("/teste", (req, res) => {
+    return res.send("Donis Teste");
+})
+
+app.listen(3000, () => console.log("Server is running NLW"))
