7cca10340df1faf1c6f6800f4492a02fe6c71f9b
donissousa <donisgtr@gmail.com>
2021-07-05 20:39:28 -0300

    typeorm create

---
+++ src/database/index.ts
@@ -0,0 +1,3 @@
+/* esse arquivo e para criar a conexão com o banco de dados sqlite */
+import { createConnection } from "typeorm";
+createConnection();
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
+import "reflect-metadata";
 import express from "express"; /* ... pontinhos sigifina que instalar as tipagens como devDepencies  @types/nomedabiblioteca nesse caso yarn add @types/express*/
+import "./database"; /* importando aquele codigo que acabei de criar para conexão do banco de dados */
 
 const app = express();
-
-app.get("/teste", (req, res) => {
-    return res.send("Donis Teste");
-})
 
 app.listen(3000, () => console.log("Server is running NLW"))
